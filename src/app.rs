b4c2d7d1c39ace87302f7d078a69f912b93d2c4e
William Bilsdon <williambilsdon@gmail.com>
2025-05-29 20:31:07 +0100

    fix new line inputs appearing on first line

--- src/app.rs
+++ src/app.rs
@@ -12,13 +12,15 @@ use crate::enigma::machine::EnigmaMachine;
 
 /// App holds the state of the application
 pub struct App {
-    /// Current value of the input box
+    // Current value of the input box
     input: String,
     encrypted_input: String,
-    /// Position of cursor in the editor area.
-    character_x_index: usize,
-    character_y_index: usize,
-    /// Current input mode
+    // Position of cursor in input
+    character_index: usize,
+    // Position of cursor in the editor area.
+    cursor_x_index: usize,
+    cursor_y_index: usize,
+    // Current input mode
     input_mode: InputMode,
     enigma: EnigmaMachine,
 }
@@ -33,30 +35,31 @@ impl App {
             input: String::new(),
             encrypted_input: String::new(),
             input_mode: InputMode::Normal,
-            character_x_index: 0,
-            character_y_index: 0,
+            character_index: 0,
+            cursor_x_index: 0,
+            cursor_y_index: 0,
             enigma,
         }
     }
 
     fn move_cursor_left(&mut self) {
-        let cursor_moved_left = self.character_x_index.saturating_sub(1);
-        self.character_x_index = self.clamp_cursor(cursor_moved_left);
+        let cursor_moved_left = self.cursor_x_index.saturating_sub(1);
+        self.cursor_x_index = self.clamp_cursor(cursor_moved_left);
     }
 
     fn move_cursor_right(&mut self) {
-        let cursor_moved_right = self.character_x_index.saturating_add(1);
-        self.character_x_index = self.clamp_cursor(cursor_moved_right);
+        let cursor_moved_right = self.cursor_x_index.saturating_add(1);
+        self.cursor_x_index = self.clamp_cursor(cursor_moved_right);
     }
 
     fn move_cursor_up(&mut self) {
-        let cursor_moved_up = self.character_y_index.saturating_sub(1);
-        self.character_y_index = self.clamp_cursor(cursor_moved_up);
+        let cursor_moved_up = self.cursor_y_index.saturating_sub(1);
+        self.cursor_y_index = self.clamp_cursor(cursor_moved_up);
     }
 
     fn move_cursor_down(&mut self) {
-        let cursor_moved_down = self.character_y_index.saturating_add(1);
-        self.character_y_index = self.clamp_cursor(cursor_moved_down);
+        let cursor_moved_down = self.cursor_y_index.saturating_add(1);
+        self.cursor_y_index = self.clamp_cursor(cursor_moved_down);
     }
 
     fn enter_char(&mut self, new_char: char) {
@@ -71,6 +74,7 @@ impl App {
 
         self.encrypted_input.insert(index, encrypted_char);
         self.move_cursor_right();
+        self.character_index += 1;
     }
 
     fn new_line(&mut self) {
@@ -78,7 +82,8 @@ impl App {
         self.input.insert(index, '\u{000a}');
         self.encrypted_input.insert(index, '\u{000a}');
         self.move_cursor_down();
-        self.character_x_index = 0;
+        self.cursor_x_index = 0;
+        self.character_index += 1;
     }
 
     /// Returns the byte index based on the character position.
@@ -88,18 +93,18 @@ impl App {
         self.input
             .char_indices()
             .map(|(i, _)| i)
-            .nth(self.character_x_index + self.character_y_index)
+            .nth(self.character_index)
             .unwrap_or(self.input.len())
     }
 
     fn delete_char(&mut self) {
-        let is_not_cursor_leftmost = self.character_x_index != 0;
-        if is_not_cursor_leftmost {
+        let is_not_cursor_top_left = self.cursor_x_index != 0 && self.cursor_y_index != 0;
+        if is_not_cursor_top_left {
             // Method "remove" is not used on the saved text for deleting the selected char.
             // Reason: Using remove on String works on bytes instead of the chars.
             // Using remove would require special care because of char boundaries.
 
-            let current_index = self.character_x_index;
+            let current_index = self.cursor_x_index;
             let from_left_to_current_index = current_index - 1;
 
             // Getting all characters before the selected character.
@@ -163,9 +168,9 @@ impl App {
             InputMode::Normal => frame.set_cursor_position(Position::new(
                 // Draw the cursor at the current position in the input field.
                 // This position is can be controlled via the left and right arrow key
-                input_area.x + self.character_x_index as u16 + 1,
+                input_area.x + self.cursor_x_index as u16 + 1,
                 // Move one line down, from the border to the input line
-                input_area.y + self.character_y_index as u16 + 1,
+                input_area.y + self.cursor_y_index as u16 + 1,
             )),
         }
     }
